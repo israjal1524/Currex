4be95f82cc84fe91ccac29db4d33d25f5b6cf5d7
Srajaltripathi <138282019+israjal1524@users.noreply.github.com>
2025-07-17 23:32:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 import requests
 
+st.markdown("""
+    <style>
+    .stApp {
+        background-image: url("https://images.unsplash.com/photo-1599010379568-14e6b3283fbe?crop=entropy&cs=srgb&fm=jpg&ixid=M3wyMTU1fDB8MXxzZWFyY2h8MzUyfHxmb3JleCUyMGV4Y2hhbmdlfGVufDB8fHx8MTc1Mjc3NTMwNXww&ixlib=rb-4.1.0&q=85");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
 st.title("CUREX")
 amount=st.number_input("Enter the Amount of Exchang in INR",min_value=1)
 target_currency=st.selectbox("Exchange to currency",["USD","CAD","PKR","JPY","EUR"])
